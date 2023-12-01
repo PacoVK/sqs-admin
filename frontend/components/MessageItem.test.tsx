62d6a06e6e6d510c27561f79d51d7764150e6d53
Pascal Euhus <pascal.euhus@gmx.de>
2023-12-01 21:34:17 +0100

    simple fake date

--- frontend/components/MessageItem.test.tsx
+++ frontend/components/MessageItem.test.tsx
@@ -6,7 +6,7 @@ describe("<MessageItem /> spec", () => {
         const view = render(<MessageItem data={{
             messageBody: "Hello",
             messageAttributes: {
-                SentTimestamp: "2021-05-01T00:00:00.000Z",
+                SentTimestamp: "fake",
             }
         }} />);
         expect(view).toMatchSnapshot();
