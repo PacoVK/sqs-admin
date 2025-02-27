672fc92d03473b61abf521fbf0404abb33a424a4
Pascal Euhus <pascal.euhus@gmx.de>
2025-02-27 20:23:43 +0100

    move to esm

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
-module.exports = {
+export default {
   testEnvironment: "./frontend/setupTests.ts",
   roots: ["<rootDir>/frontend"],
   testMatch: ["**/*.test.tsx"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": ["ts-jest", {
+      useESM: true,
+    }],
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   collectCoverage: true,
 };
